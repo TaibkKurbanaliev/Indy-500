5e5173894739be41ed2633c180a502cf118c9f91
TaibkKurbanaliev <65422168+TaibkKurbanaliev@users.noreply.github.com>
2025-03-23 22:43:39 +0400

    Add surfaces and car rotation

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,13 +1,18 @@
 using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
 
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float _speed = 1f;
-    [SerializeField] private float _angularSpeed;
-    [SerializeField] private float _friction = 0.4f;
+    [SerializeField] private float _rotationSpeed = 0.4f;
+    [SerializeField] private float _roadFriction = 0.4f;
+    [SerializeField] private float _sandFriction = 0.8f;
+    [SerializeField] private LayerMask _roadMask;
+    [SerializeField] private LayerMask _sandMask;
 
     private Rigidbody2D _rigidbody;
     private InputSystem_Actions _actions;
+    private float _friction = 0.4f;
 
     private void Awake()
     {
@@ -28,9 +33,26 @@ public class PlayerMovement : MonoBehaviour
 
     private void Move()
     {
-        var input = _actions.Player.Move.ReadValue<Vector2>().y;
-        Debug.Log(transform.forward * _speed);
-        _rigidbody.AddForce(transform.up * _speed * input * Time.fixedDeltaTime);
+        var input = _actions.Player.Move.ReadValue<Vector2>();
+        _rigidbody.linearVelocity = transform.up * _speed * input.y * Time.fixedDeltaTime;
         _rigidbody.linearDamping = _friction;
+
+        _rigidbody.angularVelocity = -input.x * _rotationSpeed;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if ((1 << collision.gameObject.layer & _sandMask) != 0)
+        {
+            _friction = _sandFriction;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if ((1 << collision.gameObject.layer & _sandMask) != 0)
+        {
+            _friction = _roadFriction;
+        }
     }
 }
